96d260c6567398cd9314d1442660574bf816776c
Benjamin Wang <wachao@vmware.com>
2023-07-11 13:29:26 +0100

    add build.sh, release.sh and Makefile
    Signed-off-by: Benjamin Wang <wachao@vmware.com>

---
+++ Makefile
@@ -0,0 +1,46 @@
+TESTFLAGS_RACE=-race=false
+ifdef ENABLE_RACE
+	TESTFLAGS_RACE=-race=true
+endif
+
+TESTFLAGS_CPU=
+ifdef CPU
+	TESTFLAGS_CPU=-cpu=$(CPU)
+endif
+TESTFLAGS = $(TESTFLAGS_RACE) $(TESTFLAGS_CPU) $(EXTRA_TESTFLAGS)
+
+TESTFLAGS_TIMEOUT=10m
+ifdef TIMEOUT
+	TESTFLAGS_TIMEOUT=$(TIMEOUT)
+endif
+
+GOFILES = $(shell find . -name \*.go)
+
+all: build
+
+.PHONY: build
+build:
+	GO_BUILD_FLAGS="${GO_BUILD_FLAGS} -v -mod=readonly" ./build.sh
+
+.PHONY: fmt
+fmt:
+	@echo "Verifying gofmt"
+	@!(gofmt -l -s -d ${GOFILES} | grep '[a-z]')
+
+	@echo "Verifying goimports"
+	@!(go run golang.org/x/tools/cmd/goimports@latest -l -d ${GOFILES} | grep '[a-z]')
+
+.PHONY: lint
+lint:
+	golangci-lint run ./...
+
+.PHONY: test
+test:
+	go test -v ${TESTFLAGS} -timeout ${TESTFLAGS_TIMEOUT} ./...
+
+clean:
+	rm -rf ./bin
+	rm -rf ./release
+	rm -rf ./.idea
+	rm -f etcd-diagnosis
+
---
+++ release.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+VER=${1:-}
+REPOSITORY="${REPOSITORY:-https://github.com/ahrtr/etcd-diagnosis.git}"
+
+if [ -z "$VER" ]; then
+  echo "Usage: ${0} VERSION" >> /dev/stderr
+  exit 255
+fi
+
+function setup_env {
+  local ver=${1}
+  local proj=${2}
+
+  if [ ! -d "${proj}" ]; then
+    git clone "${REPOSITORY}"
+  fi
+
+  pushd "${proj}" >/dev/null
+    git fetch --all
+    git checkout "${ver}"
+  popd >/dev/null
+}
+
+function package {
+  local target=${1}
+  local srcdir="${2}/bin"
+
+  local ccdir="${srcdir}/${GOOS}_${GOARCH}"
+  if [ -d "${ccdir}" ]; then
+    srcdir="${ccdir}"
+  fi
+  local ext=""
+  if [ "${GOOS}" == "windows" ]; then
+    ext=".exe"
+  fi
+  cp "${srcdir}/etcd-diagnosis" "${target}/etcd-diagnosis${ext}"
+
+  cp etcd-diagnosis/README.md "${target}"/README.md
+}
+
+function main {
+  local proj="etcd-diagnosis"
+
+  mkdir -p release
+  cd release
+  setup_env "${VER}" "${proj}"
+
+  local tarcmd=tar
+
+  for os in darwin windows linux; do
+    export GOOS=${os}
+    TARGET_ARCHS=("amd64")
+
+    if [ ${GOOS} == "linux" ]; then
+      TARGET_ARCHS+=("arm64")
+      TARGET_ARCHS+=("ppc64le")
+      TARGET_ARCHS+=("s390x")
+    fi
+
+    if [ ${GOOS} == "darwin" ]; then
+      TARGET_ARCHS+=("arm64")
+    fi
+
+    for TARGET_ARCH in "${TARGET_ARCHS[@]}"; do
+      export GOARCH=${TARGET_ARCH}
+
+      pushd ${proj} >/dev/null
+      GO_LDFLAGS="-s -w" ./build.sh
+      popd >/dev/null
+
+      TARGET="${proj}-${VER}-${GOOS}-${GOARCH}"
+      mkdir "${TARGET}"
+      package "${TARGET}" "${proj}"
+
+      if [ ${GOOS} == "linux" ]; then
+        ${tarcmd} cfz "${TARGET}.tar.gz" "${TARGET}"
+        echo "Wrote release/${TARGET}.tar.gz"
+      else
+        zip -qr "${TARGET}.zip" "${TARGET}"
+        echo "Wrote release/${TARGET}.zip"
+      fi
+    done
+  done
+
+  echo "Generating sha256sums of release artifacts."
+  ls . | grep -E '\.tar.gz$|\.zip$' | xargs shasum -a 256 > ./SHA256SUMS
+}
+
+main
+
+
